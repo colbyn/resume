8274187f1a49f33a86bf06b18f279fd17abab258
Colbyn Wadman <hello@colbyn.com>
2018-05-31 16:33:34 -0600

    Misc build helpers

---
+++ scrips/dev/start-browser.sh
@@ -0,0 +1,1 @@
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=http://127.0.0.1:5000/ --auto-open-devtools-for-tabs
---
+++ scrips/dev/start-server.sh
@@ -0,0 +1,15 @@
+# Setup - Initialization
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
+ROOT_DIR_RAW=$(dirname "$DIR")/../;
+ROOT_DIR=$(cd $ROOT_DIR_RAW && pwd)
+
+cd $ROOT_DIR
+
+
+# Pre-Exec Specific Setup
+cd ./etc/browser-target/
+
+# Exec
+http-server ./ --port 500
+
+
